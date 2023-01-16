81bf7f2b151461f82c7c1028f036ce8435799b0a
Son Chu Thai <son.chuthai@ascendcorp.com>
2023-01-16 09:44:01 +0700

    Update code for Valid Parentheses problem

---
+++ Solution/src/com/leetcode/solution/topInterviewQuestions/validParentheses.java
@@ -0,0 +1,47 @@
+package com.leetcode.solution.topInterviewQuestions;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class validParentheses {
+
+  public static void main(String[] args) {
+    if(isValid("((")){
+      System.out.print("true");
+    } else {
+      System.out.print("false");
+    }
+  }
+
+  public static boolean isValid(String s) {
+
+    if(s.length() < 2){
+      return false;
+    }
+
+    List<Character> bracketArrays = Arrays.asList('(','[','{','}',']',')');
+    LinkedList<Character> parentheses = new LinkedList<>();
+
+    for (int i = 0; i < s.length() ; i ++){
+      int index = bracketArrays.indexOf(s.charAt(i));
+      if(index > -1){
+        if(index < 3){
+          parentheses.add(s.charAt(i));
+        } else {
+          if(parentheses.isEmpty()){
+            return false;
+          }
+          Character parenthe = parentheses.getLast();
+          if(parenthe != bracketArrays.get(5-index) ){
+            return false;
+          }
+          parentheses.removeLast();
+        }
+      }
+    }
+
+    return parentheses.isEmpty();
+  }
+
+}
