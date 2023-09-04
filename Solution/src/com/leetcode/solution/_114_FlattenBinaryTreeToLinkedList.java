1e6ed6a220cb29ec8ddcdb3308b009c4125fde42
Son Chu Thai <son19101998@gmail.com>
2023-09-04 15:11:34 +0700

    Add solution for 114;146

---
+++ Solution/src/com/leetcode/solution/_114_FlattenBinaryTreeToLinkedList.java
@@ -0,0 +1,109 @@
+package com.leetcode.solution;
+
+import com.leetcode.solution.validateBST.TreeNode;
+
+public class _114_FlattenBinaryTreeToLinkedList {
+    /*
+        Given the root of a binary tree, flatten the tree into a "linked list":
+
+        The "linked list" should use the same TreeNode class where the right child pointer points to the next node in the list and the left child pointer is always null.
+        The "linked list" should be in the same order as a pre-order traversal of the binary tree.
+
+
+        Example 1:
+
+
+        Input: root = [1,2,5,3,4,null,6]
+        Output: [1,null,2,null,3,null,4,null,5,null,6]
+        Example 2:
+
+        Input: root = []
+        Output: []
+        Example 3:
+
+        Input: root = [0]
+        Output: [0]
+
+9
+        Constraints:
+
+        The number of nodes in the tree is in the range [0, 2000].
+        -100 <= Node.val <= 100
+
+
+        Follow up: Can you flatten the tree in-place (with O(1) extra space)?
+     */
+
+    public static void main(String[] args) {
+//        TreeNode _4 = new TreeNode(4);
+//        TreeNode _3 = new TreeNode(3);
+//        TreeNode _2 = new TreeNode(2,_3,_4);
+//
+//        TreeNode _6 = new TreeNode(6);
+//        TreeNode _5 = new TreeNode(5,null,_6);
+//
+//        TreeNode _1 = new TreeNode(1,_2,_5);
+
+        TreeNode _2 = new TreeNode(2);
+        TreeNode _1 = new TreeNode(1,_2,null);
+        flatten(_1);
+        System.out.println("Finish");
+    }
+
+    public static void flatten(TreeNode root) {
+        TreeNode leftestNode = flattenToRight(root, null);
+//        flatten(root.right,leftestNode);
+
+    }
+
+    private static void flattenByInOrder(TreeNode root){
+        TreeNode curr = root;
+
+        while (curr != null){
+            if(curr.left != null){
+                TreeNode running = curr.left;
+                while (running.right != null) running = running.right;
+                running.right = curr.right;
+                curr.right = curr.left;
+                curr.left = null;
+            }
+            curr = curr.right;
+        }
+    }
+
+    private static TreeNode flattenToRight(TreeNode node, TreeNode rightestNode){
+
+        if(node == null){
+            return rightestNode;
+        }
+        if(rightestNode == null){
+            rightestNode = node;
+        }
+
+        if(node.left != null){
+            rightestNode = node.left;
+        }
+
+        rightestNode = flattenToRight(node.left,rightestNode);
+
+//        flattenToRight(node.right,rightestNode);
+
+        if(node.right != null){
+            rightestNode.right = node.right;
+            if(node.left != null) {
+                node.right = node.left;
+            }
+            node.left = null;
+            rightestNode = rightestNode.right;
+        } else {
+            if(node.left != null) {
+                node.right = node.left;
+            }
+            node.left = null;
+            return rightestNode;
+        }
+
+        return flattenToRight(rightestNode,rightestNode);
+
+    }
+}
---
+++ Solution/src/com/leetcode/solution/_146_LRUCache.java
@@ -0,0 +1,106 @@
+package com.leetcode.solution;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+public class _146_LRUCache {
+
+    /*
+        Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
+
+        Implement the LRUCache class:
+
+        LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
+        int get(int key) Return the value of the key if the key exists, otherwise return -1.
+        void put(int key, int value) Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache. If the number of keys exceeds the capacity from this operation, evict the least recently used key.
+        The functions get and put must each run in O(1) average time complexity.
+
+
+
+        Example 1:
+
+        Input
+        ["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+        [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+        Output
+        [null, null, null, 1, null, -1, null, -1, 3, 4]
+
+        Explanation
+        LRUCache lRUCache = new LRUCache(2);
+        lRUCache.put(1, 1); // cache is {1=1}
+        lRUCache.put(2, 2); // cache is {1=1, 2=2}
+        lRUCache.get(1);    // return 1
+        lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+        lRUCache.get(2);    // returns -1 (not found)
+        lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+        lRUCache.get(1);    // return -1 (not found)
+        lRUCache.get(3);    // return 3
+        lRUCache.get(4);    // return 4
+
+
+        Constraints:
+
+        1 <= capacity <= 3000
+        0 <= key <= 104
+        0 <= value <= 105
+        At most 2 * 105 calls will be made to get and put.
+     */
+
+    public static void main(String[] args) {
+        LRUCache lRUCache = new LRUCache(2);
+        lRUCache.put(1, 1); // cache is {1=1}
+        lRUCache.put(2, 2); // cache is {1=1, 2=2}
+        int get_1 = lRUCache.get(1);// return 1
+        lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+        int get_2 = lRUCache.get(2);// returns -1 (not found)
+        lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+        int get_1_2 = lRUCache.get(1);// return -1 (not found)
+        int get_3 = lRUCache.get(3);// return 3
+        int get_4 = lRUCache.get(4);// return 4
+        System.out.println("Finish");
+    }
+
+    // Implement DoubleLinkedList manual will improve Performance
+
+     static class LRUCache {
+
+        Map<Integer, Integer> cacheMap;
+        LinkedList<Integer> rankedRecentlyUsed;
+        int capacity;
+
+        public LRUCache(int capacity) {
+            this.capacity = capacity;
+            cacheMap = new HashMap<>();
+            rankedRecentlyUsed = new LinkedList<>();
+        }
+
+        public int get(int key) {
+            if( cacheMap.containsKey(key) ){
+                reassignRecentKey(key);
+                return cacheMap.get(key);
+            }
+            return -1;
+        }
+
+        public void put(int key, int value) {
+            if(cacheMap.containsKey(key)){
+                reassignRecentKey(key);
+            } else {
+                rankedRecentlyUsed.addLast(key);
+            }
+            cacheMap.put(key, value);
+
+            if(cacheMap.size() > capacity){
+                Integer first = rankedRecentlyUsed.getFirst();
+                cacheMap.remove(first);
+                rankedRecentlyUsed.removeFirst();
+            }
+        }
+
+        private void reassignRecentKey(int key){
+            rankedRecentlyUsed.removeFirstOccurrence(key);
+            rankedRecentlyUsed.addLast(key);
+        }
+    }
+}
