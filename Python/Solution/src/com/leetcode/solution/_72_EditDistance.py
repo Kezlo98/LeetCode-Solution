c79c49cf892f06b67c55a4fa31d87cd3dbec8d01
CTSon <son19101998@gmail.com>
2024-04-23 10:04:53 +0700

    Refactor for java and python

--- Python/Solution/src/com/leetcode/solution/_72_EditDistance.py
+++ Python/Solution/src/com/leetcode/solution/_72_EditDistance.py
@@ -60,19 +60,24 @@ Solution:
 
 
 def minDistance(word1: str, word2: str) -> int:
-    dp = [[0 for _ in range(len(word2) + 1)] for _ in range(len(word1) + 1)]
-    for i in range(1, len(word1)):
+    n = len(word1)
+    m = len(word2)
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
+
+    for i in range(1, n + 1):
         dp[i][0] = i
-    for j in range(1 ,len(word2)):
-        dp[j][0] = j
-    for i in range(1, len(word1)):
-        for j in range(1, len(word2)):
-            if word1[i] == word2[j]:
-                dp[i][j] = dp[i-1][j-1]+1
+    for j in range(1, m + 1):
+        dp[0][j] = j
+
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if word1[i - 1] == word2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1]
             else:
-                dp[i][j] = min(dp[i][j], dp[i-1][j], dp[i][j-1]) + 1
-    return dp[-1][-1]
+                dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1
+
+    return dp[n][m]
 
 
 if __name__ == '__main__':
-    print("Result: ", minDistance("horse", "rse"))
+    print("Result: ", minDistance("horse", "ros"))
